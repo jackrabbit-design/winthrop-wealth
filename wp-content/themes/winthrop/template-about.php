1e1e8d68dcc593b15e647e7e57814fbffb090bd9
kcroakeJRD <kevin@jumpingjackrabbit.com>
2015-08-12 17:45:48 -0400

    About Template Adn Adjusted styels

---
+++ wp-content/themes/winthrop/template-about.php
@@ -0,0 +1,105 @@
+<?php 
+/*Template Name: About */
+get_header(); the_post(); ?>
+<?php echo get_template_part('inc/inc', 'banner'); ?>
+
+<section class="top-content wrapper">
+	<div class="top-wrap">
+		<p>We act as your trusted consultant and strategic partner to assist and coordinate the pursuit of your financial, retirement, and business succession goals. We work with your other specialty advisors and professionals to provide a fully integrated plan.</p>
+	</div>
+</section>
+
+<section class="main clearfix">
+	<div class="philosophy">
+		<h3>Our Philosophy</h3>
+		<h4>Clients first, always – is the bedrock of our philosophy at Winthrop Wealth Management Management.</h4>
+<p>Over 30 years ago, the founding partners established our business with one thing in mind: focus on what is best for the client and the rest will follow. At Winthrop Wealth Management, we believe that helping clients discover and pursue financial success and freedom requires putting them front and center, providing them with continual and comprehensive attention. That is our purpose and our passion.</p>
+<p>Each of our clients is unique. They have diverse backgrounds and aspirations; lead different lives whether as successful entrepreneurs, athletes, physicians, dentists, or other professionals; and have specific and diverse needs.</p>
+<blockquote>We are committed to getting to know our clients personally, staying informed and involved in their careers, business ventures, and life developments. </blockquote>
+<p>Our long-term clients, many of whom have been with us for over thirty years, are a testament to the firm’s business philosophy, family boutique culture, and its strong value system.</p>
+<p>Winthrop Wealth Management believes that building trust and cultivating long-term relationships are essential to providing high-quality wealth management services. Consistent with the emphasis we place on building long-term and productive relationships is our team’s multi-generational expertise, which ensures service continuity and personalized attention across generations.</p>
+	</div>
+	<div class="cornerstones">
+		<h3>Cornerstones Of Our Business</h3>
+		<ul>
+			<li>
+				<div class="circle"></div>
+				<div class="contnet">
+					<h5>Client-First Approach.</h5>
+					<div class="hidden">
+						<p>Our clients are our partners and often become good friends as our relationship develops over time.</p>
+					</div>
+				</div>
+			</li>
+			<li>
+				<div class="circle"></div>
+				<div class="contnet">
+					<h5>Client-First Approach.</h5>
+					<div class="hidden">
+						<p>Our clients are our partners and often become good friends as our relationship develops over time.</p>
+					</div>
+				</div>
+			</li>
+			<li>
+				<div class="circle"></div>
+				<div class="contnet">
+					<h5>Client-First Approach.</h5>
+					<div class="hidden">
+						<p>Our clients are our partners and often become good friends as our relationship develops over time.</p>
+					</div>
+				</div>
+			</li>
+			<li>
+				<div class="circle"></div>
+				<div class="contnet">
+					<h5>Client-First Approach.</h5>
+					<div class="hidden">
+						<p>Our clients are our partners and often become good friends as our relationship develops over time.</p>
+					</div>
+				</div>
+			</li>
+			<li>
+				<div class="circle"></div>
+				<div class="contnet">
+					<h5>Client-First Approach.</h5>
+					<div class="hidden">
+						<p>Our clients are our partners and often become good friends as our relationship develops over time.</p>
+					</div>
+				</div>
+			</li>
+			<li>
+				<div class="circle"></div>
+				<div class="contnet">
+					<h5>Client-First Approach.</h5>
+					<div class="hidden">
+						<p>Our clients are our partners and often become good friends as our relationship develops over time.</p>
+					</div>
+				</div>
+			</li>
+		</ul>
+	</div>
+</section>
+<section class="by-numbers">
+	<div class="wrapper">
+		<h2>Winthrop By the Numbers</h2>
+		<ul>
+			<li>
+				<div>1985</div>
+				<span>Founded by Earl and Mark Winthrop</span>
+			</li>
+			<li>
+				<div>$1.3<small>BILLION</small></div>
+				<span>In client assets under managment</span>
+			</li>
+			<li>
+				<div>99%</div>
+				<span>Client retnention over th last 25+ years</span>
+			</li>
+			<li>
+				<div>25<small>years</small></div>
+				<span>Assoicate with LPL Financial the nations leading broker to independent advisors</span>
+			</li>
+		</ul>
+	</div>
+</section>
+<?php get_footer(); ?>
